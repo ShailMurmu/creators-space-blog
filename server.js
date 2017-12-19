60039fa0666b13602aa420ee0b094871929795fc
ShailMurmu <shailmurmu1996@gmail.com>
2017-12-19 23:14:44 +0530

    Landing page completed

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var root = path.join(__dirname);
 server.use(express.static(__dirname));
 server.set('view engine', 'hbs');
 
-server.get('/test1',(req, res) => {
-    res.sendFile(path.join(root,'/public/test.html'));
+server.get('/test1', (req, res) => {
+  res.sendFile(path.join(root, '/public/test.html'));
 });
 
 server.get('/test2', (req, res) => {
-    res.render('test.hbs');
+  res.render('test.hbs');
 });
 
 server.listen(3000, () => {
-    console.log('server started on port 3000');
+  console.log('server started on port 3000');
 });
